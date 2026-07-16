4d56e703a9ded12c574bc171ec574b8286b211a5
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-16 17:56:23 +0800

    Integrate synchronous SRAM buffer into DMA datapath

--- rtl/axi_buf_dma.sv
+++ rtl/axi_buf_dma.sv
@@ -62,9 +62,9 @@ module axi_buf_dma #(
   localparam logic [1:0] AXI_RESP_OKAY = 2'b00;
   localparam int unsigned AXI_BYTES = AXI_DATA_WIDTH/8;
   localparam int unsigned BUFFER_BYTES = (1 << BUFFER_ADDR_WIDTH) * 4;
+  localparam int unsigned TIMEOUT_W = (AXI_TIMEOUT_CYCLES <= 1) ? 1 : $clog2(AXI_TIMEOUT_CYCLES);
 
   dma_state_t state;
-
   logic [AXI_ADDR_WIDTH-1:0] current_addr;
   logic [9:0] remaining_bytes;
   logic [9:0] buffer_byte_index;
@@ -72,43 +72,40 @@ module axi_buf_dma #(
   logic [7:0] burst_beats;
   logic [12:0] burst_bytes;
   logic [7:0] beat_index;
-  logic [AXI_DATA_WIDTH-1:0] wdata_reg;
-  logic [AXI_DATA_WIDTH/8-1:0] wstrb_reg;
+
+  logic [AXI_DATA_WIDTH-1:0] wdata_reg, wdata_stage, wdata_with_byte;
+  logic [AXI_DATA_WIDTH/8-1:0] wstrb_reg, packed_wstrb;
   logic wlast_reg;
-  logic [3:0] lane_start;
-  logic [3:0] bytes_this_beat;
-  logic [AXI_DATA_WIDTH-1:0] packed_wdata;
-  logic [AXI_DATA_WIDTH/8-1:0] packed_wstrb;
+  logic [3:0] lane_start, bytes_this_beat, w_load_count;
+  logic [7:0] sram_read_byte;
+  logic [9:0] w_source_byte_index;
 
-  logic [AXI_DATA_WIDTH-1:0] buffer_dma_read_data;
-  logic buffer_dma_write_enable;
-  logic [BUFFER_ADDR_WIDTH+1:0] buffer_dma_write_addr;
-  logic [AXI_DATA_WIDTH-1:0] buffer_dma_write_data;
-  logic [AXI_DATA_WIDTH/8-1:0] buffer_dma_write_strb;
-  logic read_beat_accept;
+  logic [AXI_DATA_WIDTH-1:0] rdata_reg;
+  logic rlast_reg;
+  logic [3:0] r_lane_start, r_payload_count, r_payload_index;
+  logic [31:0] read_word_stage, read_word_with_byte;
+  logic [7:0] current_r_byte;
+  logic r_word_write;
 
-  integer unsigned timeout_count;
-  logic wait_state;
-  logic forward_progress;
-  logic timeout_hit;
-  logic done_event;
-  logic error_event;
+  logic [BUFFER_ADDR_WIDTH-1:0] sram_address;
+  logic sram_wr_en, sram_csn;
+  logic [31:0] sram_din, sram_dout;
+
+  logic [TIMEOUT_W-1:0] timeout_count;
+  logic wait_state, forward_progress, timeout_hit;
+  logic done_event, error_event;
 
   function automatic [12:0] calc_burst_bytes(
     input logic [AXI_ADDR_WIDTH-1:0] addr,
     input logic [9:0] bytes_left
   );
-    integer unsigned page_bytes;
-    integer unsigned max_payload;
-    integer unsigned selected_bytes;
+    integer unsigned page_bytes, max_payload, selected_bytes;
     begin
       page_bytes = 4096 - addr[11:0];
       max_payload = AXI_MAX_BURST_LEN * AXI_BYTES - addr[2:0];
       selected_bytes = bytes_left;
-      if (selected_bytes > page_bytes)
-        selected_bytes = page_bytes;
-      if (selected_bytes > max_payload)
-        selected_bytes = max_payload;
+      if (selected_bytes > page_bytes) selected_bytes = page_bytes;
+      if (selected_bytes > max_payload) selected_bytes = max_payload;
       calc_burst_bytes = selected_bytes[12:0];
     end
   endfunction
@@ -125,25 +122,18 @@ module axi_buf_dma #(
   endfunction
 
   axi_buf_dma_buffer #(
-    .ADDR_WIDTH     (BUFFER_ADDR_WIDTH),
-    .DMA_DATA_WIDTH (AXI_DATA_WIDTH)
+    .ADDR_WIDTH(BUFFER_ADDR_WIDTH),
+    .DATA_WIDTH(32)
   ) u_local_buffer (
-    .clk                 (clk),
-    .rst_n               (rst_n),
-    .host_enable         (!dma_busy),
-    .host_addr           (buf_addr),
-    .host_wdata          (buf_din),
-    .host_we             (buf_wr_en),
-    .host_csn            (buf_csn),
-    .host_rdata          (buf_dout),
-    .dma_read_byte_addr  (buffer_byte_index[BUFFER_ADDR_WIDTH+1:0]),
-    .dma_read_data       (buffer_dma_read_data),
-    .dma_write_enable    (buffer_dma_write_enable),
-    .dma_write_byte_addr (buffer_dma_write_addr),
-    .dma_write_data      (buffer_dma_write_data),
-    .dma_write_strb      (buffer_dma_write_strb)
+    .clk(clk),
+    .address(sram_address),
+    .wr_en(sram_wr_en),
+    .csn(sram_csn),
+    .din(sram_din),
+    .dout(sram_dout)
   );
 
+  assign buf_dout = sram_dout;
   assign dma_irq = (irq_done_status & irq_done_enable) |
                    (irq_error_status & irq_error_enable);
 
@@ -155,37 +145,49 @@ module axi_buf_dma #(
       bytes_this_beat = AXI_BYTES - lane_start;
   end
 
-  integer pack_lane;
-  integer pack_offset;
+  integer strb_lane;
   always_comb begin
-    packed_wdata = '0;
     packed_wstrb = '0;
-    for (pack_lane = 0; pack_lane < AXI_BYTES; pack_lane = pack_lane + 1) begin
-      pack_offset = pack_lane - lane_start;
-      if ((pack_lane >= lane_start) && (pack_offset < remaining_bytes)) begin
-        packed_wdata[pack_lane*8 +: 8] = buffer_dma_read_data[pack_offset*8 +: 8];
-        packed_wstrb[pack_lane] = 1'b1;
-      end
-    end
+    for (strb_lane = 0; strb_lane < AXI_BYTES; strb_lane = strb_lane + 1)
+      if ((strb_lane >= lane_start) && ((strb_lane-lane_start) < bytes_this_beat))
+        packed_wstrb[strb_lane] = 1'b1;
   end
 
-  assign read_beat_accept = (state == DMA_R_DATA) && m_axi_rvalid &&
-                            (m_axi_rresp == AXI_RESP_OKAY) &&
-                            !(m_axi_rlast && (beat_index != (burst_beats - 1'b1))) &&
-                            !(!m_axi_rlast && (beat_index == (burst_beats - 1'b1)));
+  assign w_source_byte_index = buffer_byte_index + w_load_count;
+  assign sram_read_byte = sram_dout[w_source_byte_index[1:0]*8 +: 8];
 
-  integer read_lane;
-  integer read_offset;
   always_comb begin
-    buffer_dma_write_enable = read_beat_accept;
-    buffer_dma_write_addr = buffer_byte_index[BUFFER_ADDR_WIDTH+1:0];
-    buffer_dma_write_data = '0;
-    buffer_dma_write_strb = '0;
-    for (read_lane = 0; read_lane < AXI_BYTES; read_lane = read_lane + 1) begin
-      read_offset = read_lane - lane_start;
-      if ((read_lane >= lane_start) && (read_offset < remaining_bytes)) begin
-        buffer_dma_write_data[read_offset*8 +: 8] = m_axi_rdata[read_lane*8 +: 8];
-        buffer_dma_write_strb[read_offset] = 1'b1;
+    wdata_with_byte = wdata_stage;
+    wdata_with_byte[(lane_start+w_load_count)*8 +: 8] = sram_read_byte;
+  end
+
+  assign current_r_byte = rdata_reg[(r_lane_start+r_payload_index)*8 +: 8];
+  always_comb begin
+    read_word_with_byte = (buffer_byte_index[1:0] == 2'd0) ? 32'd0 : read_word_stage;
+    read_word_with_byte[buffer_byte_index[1:0]*8 +: 8] = current_r_byte;
+    r_word_write = (state == DMA_R_UNPACK) &&
+                   ((buffer_byte_index[1:0] == 2'd3) || (remaining_bytes == 10'd1));
+  end
+
+  always_comb begin
+    sram_address = buf_addr;
+    sram_wr_en = buf_wr_en;
+    sram_csn = buf_csn;
+    sram_din = buf_din;
+
+    if (dma_busy) begin
+      sram_address = '0;
+      sram_wr_en = 1'b0;
+      sram_csn = 1'b1;
+      sram_din = '0;
+      if (state == DMA_W_BUF_REQ) begin
+        sram_address = w_source_byte_index[BUFFER_ADDR_WIDTH+1:2];
+        sram_csn = 1'b0;
+      end else if (r_word_write) begin
+        sram_address = buffer_byte_index[BUFFER_ADDR_WIDTH+1:2];
+        sram_wr_en = 1'b1;
+        sram_csn = 1'b0;
+        sram_din = read_word_with_byte;
       end
     end
   end
@@ -223,63 +225,65 @@ module axi_buf_dma #(
   end
 
   assign timeout_hit = wait_state && !forward_progress &&
-                       (timeout_count >= (AXI_TIMEOUT_CYCLES-1));
+                       (timeout_count >= AXI_TIMEOUT_CYCLES-1);
 
   always_ff @(posedge clk) begin
     if (!rst_n)
-      timeout_count <= 0;
+      timeout_count <= '0;
     else if (!wait_state || forward_progress)
-      timeout_count <= 0;
+      timeout_count <= '0;
     else if (!timeout_hit)
-      timeout_count <= timeout_count + 1;
+      timeout_count <= timeout_count + 1'b1;
   end
 
   always_ff @(posedge clk) begin
     if (!rst_n) begin
-      irq_done_status  <= 1'b0;
+      irq_done_status <= 1'b0;
       irq_error_status <= 1'b0;
     end else begin
-      if (done_event)
-        irq_done_status <= 1'b1;
-      else if (irq_done_clear)
-        irq_done_status <= 1'b0;
-      if (error_event)
-        irq_error_status <= 1'b1;
-      else if (irq_error_clear)
-        irq_error_status <= 1'b0;
+      if (done_event) irq_done_status <= 1'b1;
+      else if (irq_done_clear) irq_done_status <= 1'b0;
+      if (error_event) irq_error_status <= 1'b1;
+      else if (irq_error_clear) irq_error_status <= 1'b0;
     end
   end
 
   always_ff @(posedge clk) begin
     if (!rst_n) begin
-      state             <= DMA_IDLE;
-      dma_ready         <= 1'b0;
-      dma_busy          <= 1'b0;
-      dma_error         <= 1'b0;
-      dma_error_code    <= DMA_ERR_NONE;
-      timeout_status    <= 1'b0;
-      current_addr      <= '0;
-      remaining_bytes   <= '0;
+      state <= DMA_IDLE;
+      dma_ready <= 1'b0;
+      dma_busy <= 1'b0;
+      dma_error <= 1'b0;
+      dma_error_code <= DMA_ERR_NONE;
+      timeout_status <= 1'b0;
+      current_addr <= '0;
+      remaining_bytes <= '0;
       buffer_byte_index <= '0;
-      direction_write   <= 1'b0;
-      burst_beats       <= 8'd1;
-      burst_bytes       <= 13'd1;
-      beat_index        <= '0;
-      wdata_reg         <= '0;
-      wstrb_reg         <= '0;
-      wlast_reg         <= 1'b0;
-      done_event        <= 1'b0;
-      error_event       <= 1'b0;
-    end else begin
-      done_event  <= 1'b0;
+      direction_write <= 1'b0;
+      burst_beats <= 8'd1;
+      burst_bytes <= 13'd1;
+      beat_index <= '0;
+      wdata_reg <= '0;
+      wdata_stage <= '0;
+      wstrb_reg <= '0;
+      wlast_reg <= 1'b0;
+      w_load_count <= '0;
+      rdata_reg <= '0;
+      rlast_reg <= 1'b0;
+      r_lane_start <= '0;
+      r_payload_count <= '0;
+      r_payload_index <= '0;
+      read_word_stage <= '0;
+      done_event <= 1'b0;
       error_event <= 1'b0;
-
-      if (!dma_start && !dma_busy)
-        dma_ready <= 1'b0;
+    end else begin
+      done_event <= 1'b0;
+      error_event <= 1'b0;
+      if (!dma_start && !dma_busy) dma_ready <= 1'b0;
 
       if (timeout_hit) begin
-        state          <= DMA_ERROR;
-        dma_error      <= 1'b1;
+        state <= DMA_ERROR;
+        dma_error <= 1'b1;
         dma_error_code <= DMA_ERR_TIMEOUT;
         timeout_status <= 1'b1;
       end else begin
@@ -287,133 +291,146 @@ module axi_buf_dma #(
           DMA_IDLE: begin
             dma_busy <= 1'b0;
             if (dma_start && !dma_ready) begin
-              current_addr      <= dma_sa;
-              remaining_bytes   <= {1'b0, dma_length} + 10'd1;
+              current_addr <= dma_sa;
+              remaining_bytes <= {1'b0, dma_length} + 10'd1;
               buffer_byte_index <= 10'd0;
-              direction_write   <= dma_rw;
-              dma_error         <= 1'b0;
-              dma_error_code    <= DMA_ERR_NONE;
-              timeout_status    <= 1'b0;
-              dma_busy          <= 1'b1;
-              state             <= DMA_CHECK;
+              direction_write <= dma_rw;
+              dma_error <= 1'b0;
+              dma_error_code <= DMA_ERR_NONE;
+              timeout_status <= 1'b0;
+              dma_busy <= 1'b1;
+              read_word_stage <= '0;
+              state <= DMA_CHECK;
             end
           end
 
           DMA_CHECK: begin
             if (({1'b0, dma_length} + 10'd1) > BUFFER_BYTES) begin
-              dma_error      <= 1'b1;
+              dma_error <= 1'b1;
               dma_error_code <= DMA_ERR_ALIGN;
-              state          <= DMA_ERROR;
-            end else begin
-              state <= DMA_PREP;
-            end
+              state <= DMA_ERROR;
+            end else state <= DMA_PREP;
           end
 
           DMA_PREP: begin
             burst_bytes <= calc_burst_bytes(current_addr, remaining_bytes);
-            burst_beats <= calc_burst_beats(current_addr,
-                             calc_burst_bytes(current_addr, remaining_bytes));
+            burst_beats <= calc_burst_beats(current_addr, calc_burst_bytes(current_addr, remaining_bytes));
             beat_index <= 8'd0;
-            if (direction_write)
-              state <= DMA_W_AW;
-            else
-              state <= DMA_R_AR;
+            if (direction_write) state <= DMA_W_AW;
+            else state <= DMA_R_AR;
           end
 
-          DMA_W_AW: begin
-            if (m_axi_awready)
-              state <= DMA_W_LOAD;
+          DMA_W_AW: if (m_axi_awready) begin
+            wdata_stage <= '0;
+            w_load_count <= 4'd0;
+            state <= DMA_W_BUF_REQ;
           end
 
-          DMA_W_LOAD: begin
-            wdata_reg <= packed_wdata;
-            wstrb_reg <= packed_wstrb;
-            wlast_reg <= (beat_index == (burst_beats - 1'b1));
-            state <= DMA_W_SEND;
-          end
+          DMA_W_BUF_REQ: state <= DMA_W_BUF_CAPTURE;
 
-          DMA_W_SEND: begin
-            if (m_axi_wready) begin
-              remaining_bytes   <= remaining_bytes - bytes_this_beat;
-              buffer_byte_index <= buffer_byte_index + bytes_this_beat;
-              if (wlast_reg)
-                state <= DMA_W_RESP;
-              else begin
-                beat_index <= beat_index + 1'b1;
-                state <= DMA_W_LOAD;
-              end
+          DMA_W_BUF_CAPTURE: begin
+            wdata_stage <= wdata_with_byte;
+            if (w_load_count == bytes_this_beat-1'b1) begin
+              wdata_reg <= wdata_with_byte;
+              wstrb_reg <= packed_wstrb;
+              wlast_reg <= (beat_index == burst_beats-1'b1);
+              state <= DMA_W_SEND;
+            end else begin
+              w_load_count <= w_load_count + 1'b1;
+              state <= DMA_W_BUF_REQ;
             end
           end
 
-          DMA_W_RESP: begin
-            if (m_axi_bvalid) begin
-              if (m_axi_bresp != AXI_RESP_OKAY) begin
-                dma_error      <= 1'b1;
-                dma_error_code <= DMA_ERR_BRESP;
-                state          <= DMA_ERROR;
-              end else if (remaining_bytes == 0) begin
-                state <= DMA_DONE;
-              end else begin
-                current_addr <= current_addr + burst_bytes;
-                state <= DMA_PREP;
-              end
+          DMA_W_SEND: if (m_axi_wready) begin
+            remaining_bytes <= remaining_bytes - bytes_this_beat;
+            buffer_byte_index <= buffer_byte_index + bytes_this_beat;
+            if (wlast_reg) state <= DMA_W_RESP;
+            else begin
+              beat_index <= beat_index + 1'b1;
+              wdata_stage <= '0;
+              w_load_count <= '0;
+              state <= DMA_W_BUF_REQ;
             end
           end
 
-          DMA_R_AR: begin
-            if (m_axi_arready)
-              state <= DMA_R_DATA;
+          DMA_W_RESP: if (m_axi_bvalid) begin
+            if (m_axi_bresp != AXI_RESP_OKAY) begin
+              dma_error <= 1'b1;
+              dma_error_code <= DMA_ERR_BRESP;
+              state <= DMA_ERROR;
+            end else if (remaining_bytes == 0) state <= DMA_DONE;
+            else begin
+              current_addr <= current_addr + burst_bytes;
+              state <= DMA_PREP;
+            end
           end
 
-          DMA_R_DATA: begin
-            if (m_axi_rvalid) begin
-              if (m_axi_rresp != AXI_RESP_OKAY) begin
-                dma_error      <= 1'b1;
-                dma_error_code <= DMA_ERR_RRESP;
-                state          <= DMA_ERROR;
-              end else if (m_axi_rlast && (beat_index != (burst_beats - 1'b1))) begin
-                dma_error      <= 1'b1;
-                dma_error_code <= DMA_ERR_RLAST_EARLY;
-                state          <= DMA_ERROR;
-              end else if (!m_axi_rlast && (beat_index == (burst_beats - 1'b1))) begin
-                dma_error      <= 1'b1;
-                dma_error_code <= DMA_ERR_RLAST_MISSING;
-                state          <= DMA_ERROR;
-              end else begin
-                remaining_bytes   <= remaining_bytes - bytes_this_beat;
-                buffer_byte_index <= buffer_byte_index + bytes_this_beat;
-                if (m_axi_rlast) begin
-                  if (remaining_bytes <= bytes_this_beat)
-                    state <= DMA_DONE;
-                  else begin
-                    current_addr <= current_addr + burst_bytes;
-                    state <= DMA_PREP;
-                  end
-                end else begin
-                  beat_index <= beat_index + 1'b1;
+          DMA_R_AR: if (m_axi_arready) state <= DMA_R_DATA;
+
+          DMA_R_DATA: if (m_axi_rvalid) begin
+            if (m_axi_rresp != AXI_RESP_OKAY) begin
+              dma_error <= 1'b1;
+              dma_error_code <= DMA_ERR_RRESP;
+              state <= DMA_ERROR;
+            end else if (m_axi_rlast && (beat_index != burst_beats-1'b1)) begin
+              dma_error <= 1'b1;
+              dma_error_code <= DMA_ERR_RLAST_EARLY;
+              state <= DMA_ERROR;
+            end else if (!m_axi_rlast && (beat_index == burst_beats-1'b1)) begin
+              dma_error <= 1'b1;
+              dma_error_code <= DMA_ERR_RLAST_MISSING;
+              state <= DMA_ERROR;
+            end else begin
+              rdata_reg <= m_axi_rdata;
+              rlast_reg <= m_axi_rlast;
+              r_lane_start <= lane_start;
+              r_payload_count <= bytes_this_beat;
+              r_payload_index <= '0;
+              state <= DMA_R_UNPACK;
+            end
+          end
+
+          DMA_R_UNPACK: begin
+            read_word_stage <= read_word_with_byte;
+            remaining_bytes <= remaining_bytes - 1'b1;
+            buffer_byte_index <= buffer_byte_index + 1'b1;
+            if ((buffer_byte_index[1:0] == 2'd3) || (remaining_bytes == 10'd1))
+              read_word_stage <= '0;
+
+            if (r_payload_index == r_payload_count-1'b1) begin
+              if (rlast_reg) begin
+                if (remaining_bytes == 10'd1) state <= DMA_DONE;
+                else begin
+                  current_addr <= current_addr + burst_bytes;
+                  state <= DMA_PREP;
                 end
+              end else begin
+                beat_index <= beat_index + 1'b1;
+                state <= DMA_R_DATA;
               end
+            end else begin
+              r_payload_index <= r_payload_index + 1'b1;
             end
           end
 
           DMA_DONE: begin
-            dma_busy   <= 1'b0;
-            dma_ready  <= 1'b1;
+            dma_busy <= 1'b0;
+            dma_ready <= 1'b1;
             done_event <= 1'b1;
-            state      <= DMA_IDLE;
+            state <= DMA_IDLE;
           end
 
           DMA_ERROR: begin
-            dma_busy    <= 1'b0;
-            dma_ready   <= 1'b1;
+            dma_busy <= 1'b0;
+            dma_ready <= 1'b1;
             error_event <= 1'b1;
-            state       <= DMA_IDLE;
+            state <= DMA_IDLE;
           end
 
           default: begin
-            dma_error      <= 1'b1;
+            dma_error <= 1'b1;
             dma_error_code <= DMA_ERR_ILLEGAL_STATE;
-            state          <= DMA_ERROR;
+            state <= DMA_ERROR;
           end
         endcase
       end
